3b446b7e18731570cdadbbf0a5fafe66c7485bff
peabreu <paulo.abreu@ci.uc.pt>
2022-05-24 16:39:36 +0100

    Added license information

--- scripts/mol2itp.py
+++ scripts/mol2itp.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+# Paulo E. Abreu (c) paulo.abreu@ci.uc.pt
+# 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>
+#
 # This small script creates a mol2 file with the charges from the gromacs topology
 # It prints the output file to the standard output
 # Use chimera to convert the gro file to the mol2 format
